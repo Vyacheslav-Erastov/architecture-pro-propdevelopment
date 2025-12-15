b4ac2ebeed5ebb239a513223e30d7ed444368f8d
Vyacheslav-Erastov <yurkuserastov228@gmail.com>
2025-12-16 00:16:20 +0300

    [FEATURE] Complete fifth task

---
+++ Task5/init.sh
@@ -0,0 +1,11 @@
+kubectl run front-end-app --image=nginx --labels=role=front-end --expose --port=80
+kubectl run back-end-api-app --image=nginx --labels=role=back-end-api --expose --port=80  
+kubectl run admin-front-end-app --image=nginx --labels=role=admin-front-end --expose --port=80
+kubectl run admin-back-end-api-app --image=nginx --labels=role=admin-back-end-api --expose --port=80
+
+kubectl apply -f non-admin-api-allow.yaml
+
+kubectl get netpol
+kubectl get pods --show-labels
+
+kubectl run test-$RANDOM --rm -i -t --image=alpine -- sh / # wget -qO- --timeout=2 http://back-end-api-app
